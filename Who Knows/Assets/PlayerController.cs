f2c305466159201e8733888e2d2c8ee45e4057f3
Duncan Evans <duncan3242@outlook.com>
2018-04-17 22:18:14 -0300

    Turning and started camera follow

---
+++ Who Knows/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float TurnSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)){
+			transform.Rotate (Vector3.forward * TurnSpeed);
+		}
+		if (Input.GetKey(KeyCode.D)){
+			transform.Rotate (Vector3.forward * -TurnSpeed);
+		}
+	}
+}
