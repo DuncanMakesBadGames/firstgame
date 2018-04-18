0a0ca21d951c2f62c28b4694ea7d30e31b04fa57
Angussssssssss <38479406+Angussssssssss@users.noreply.github.com>
2018-04-18 14:37:39 -0300

    Aim

---
+++ Who Knows/Assets/Scripts/TankNoseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankNoseController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePositionVector3 = new Vector3(Input.mousePosition.x,Input.mousePosition.y,0);	
+		mousePositionVector3 = Camera.main.ScreenToWorldPoint(mousePositionVector3);
+
+		Vector3 targetdir = mousePositionVector3 -transform.position;
+		transform.rotation= Quaternion.LookRotation(Vector3.forward,targetdir);
+	}
+}
