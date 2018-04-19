f1af2c9d52683b6c035df0c4e1382c7adb44cb7d
Angussssssssss <38479406+Angussssssssss@users.noreply.github.com>
2018-04-19 17:56:49 -0300

    Destruction

---
+++ Who Knows/Assets/Scripts/Destroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public GameObject Boom;
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.gameObject.CompareTag ("Boollet")) {
+
+			Destroy (gameObject);
+			Destroy (other.gameObject);
+
+			GameObject instance = GameObject.Instantiate (Boom) as GameObject;
+			instance.transform.position = transform.position;
+		}
+	}
+}
