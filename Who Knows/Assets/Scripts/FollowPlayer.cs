7d7c04449276157e53d745699aaae372f4ce866a
Duncan Evans <duncan3242@outlook.com>
2018-04-19 10:50:51 -0300

    Camera Control
    Scroll wheel to zoom
    middle click to reset

--- Who Knows/Assets/Scripts/FollowPlayer.cs
+++ Who Knows/Assets/Scripts/FollowPlayer.cs
@@ -7,7 +7,25 @@ public class FollowPlayer : MonoBehaviour {
 	public Transform TankButt;
 	public Vector3 Offset;
 
+
 	void Update () {
 		transform.position = new Vector3(TankButt.position.x + Offset.x, TankButt.position.y + Offset.y, Offset.z);
+
+		float ScrollWheel = Input.GetAxis ("Mouse ScrollWheel");
+
+		if(ScrollWheel < 0){
+			UnityEngine.Camera.main.orthographicSize += 0.1f * UnityEngine.Camera.main.orthographicSize;
+		}
+		if(ScrollWheel > 0){
+			UnityEngine.Camera.main.orthographicSize -= 0.1f * UnityEngine.Camera.main.orthographicSize;
+		}
+
+		if (UnityEngine.Camera.main.orthographicSize < 0.5f) {
+			UnityEngine.Camera.main.orthographicSize = 0.5f;
+		}
+		if (Input.GetMouseButtonDown (2)) {
+			UnityEngine.Camera.main.orthographicSize = 8.0f;
+		}
+
 	}
 }
